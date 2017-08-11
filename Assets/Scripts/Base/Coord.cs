852f0cc77aa673aacabd251f13c6ecb12622bef6
김대원 <eodnjs2998@gmail.com>
2017-08-11 23:10:26 +0900

    Coord space to tab

--- Assets/Scripts/Base/Coord.cs
+++ Assets/Scripts/Base/Coord.cs
@@ -2,16 +2,24 @@ using UnityEngine;
 
 public struct Coord
 {
-    public int x;
-    public int y;
+	public int x;
+	public int y;
 
-    public static Coord operator +(Coord a, Coord b)
-    {
-        return default(Coord);
-    }
+	public Coord(int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
 
-    public static Coord Round(Vector3 position)
-    {
-        return default(Coord);
-    }
+	public static Coord operator +(Coord a, Coord b)
+	{
+		return new Coord(a.x + b.x, a.y + b.y);
+	}
+
+	public static Coord Round(Vector3 position)
+	{
+		var x = Mathf.RoundToInt(position.x);
+		var y = Mathf.RoundToInt(position.y);
+		return new Coord(x, y);
+	}
 }
