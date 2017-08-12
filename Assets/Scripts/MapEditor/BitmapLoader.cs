6c3541daa8d8e37930041527d441576237f2fe2f
박정수 <alszmtm@gmail.com>
2017-08-12 19:23:42 +0900

    비트맵 로더 버그 수정, 맵 이미지 파일들 통로 2칸으로 수정

--- Assets/Scripts/MapEditor/BitmapLoader.cs
+++ Assets/Scripts/MapEditor/BitmapLoader.cs
@@ -13,6 +13,7 @@ public class BitmapLoader : MonoBehaviour
 	public void LoadBitmap()
 	{
 		HashSet<Coord> coords = new HashSet<Coord>();
+		HashSet<Coord> wallCoords = new HashSet<Coord>();
 		for (int x = 0; x < texture.width; x++)
 		{
 			for (int y = 0; y < texture.height; y++)
@@ -26,14 +27,14 @@ public class BitmapLoader : MonoBehaviour
 		foreach (var coord in coords)
 		{
 			SpawnFloor(coord.x, coord.y, newMap);
-			SpawnWall(coords, coord.x + 1, coord.y, newMap);
-			SpawnWall(coords, coord.x, coord.y + 1, newMap);
-			SpawnWall(coords, coord.x - 1, coord.y, newMap);
-			SpawnWall(coords, coord.x, coord.y - 1, newMap);
-			SpawnWall(coords, coord.x + 1, coord.y + 1, newMap);
-			SpawnWall(coords, coord.x + 1, coord.y - 1, newMap);
-			SpawnWall(coords, coord.x - 1, coord.y + 1, newMap);
-			SpawnWall(coords, coord.x - 1, coord.y - 1, newMap);
+			SpawnWall(coords, wallCoords, coord.x + 1, coord.y, newMap);
+			SpawnWall(coords, wallCoords, coord.x, coord.y + 1, newMap);
+			SpawnWall(coords, wallCoords, coord.x - 1, coord.y, newMap);
+			SpawnWall(coords, wallCoords, coord.x, coord.y - 1, newMap);
+			SpawnWall(coords, wallCoords, coord.x + 1, coord.y + 1, newMap);
+			SpawnWall(coords, wallCoords, coord.x + 1, coord.y - 1, newMap);
+			SpawnWall(coords, wallCoords, coord.x - 1, coord.y + 1, newMap);
+			SpawnWall(coords, wallCoords, coord.x - 1, coord.y - 1, newMap);
 		}
 	}
 	void SpawnFloor(int x, int y, Transform newMap)
@@ -44,9 +45,10 @@ public class BitmapLoader : MonoBehaviour
 		spawned.transform.position = new Vector3(x, -1, y);
 		spawned.GetComponentInChildren<MeshFilter>().mesh = floorModels[Random.Range(0, floorModels.Length)];
 	}
-	void SpawnWall(HashSet<Coord> coordSet, int x, int y, Transform newMap)
+	void SpawnWall(HashSet<Coord> coordSet, HashSet<Coord> walls, int x, int y, Transform newMap)
 	{
-		if (coordSet.Any(c => c.x == x && c.y == y)) return;
+		if (coordSet.Any(c => c.x == x && c.y == y) || walls.Any(c => c.x == x && c.y == y)) return;
+		walls.Add(new Coord(x, y));
 		Debug.Log(string.Format("Wall At {0}, {1}", x, y));
 		var spawned = Instantiate<GameObject>(wall);
 		spawned.transform.SetParent(newMap);
