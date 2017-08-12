2e670c36acbe894f529e340265e28a9eba6c4e00
박정수 <alszmtm@gmail.com>
2017-08-13 01:16:13 +0900

    캐릭터 애니메이션 보조 스크립트 추가

---
+++ Assets/Scripts/Animation/CharacterAnimation.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class CharacterAnimation : MonoBehaviour
+{
+	Character me;
+	Animator anim;
+	AttackContext attackContext;
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+		me = GetComponentInParent<Character>();
+		anim = GetComponent<Animator>();
+	}
+
+	public void Attack(CharacterAnimation target, bool successful, string trigger = "Attack")
+	{
+		anim.SetTrigger(trigger);
+		attackContext = new AttackContext();
+		attackContext.target = target;
+		attackContext.successful = successful;
+	}
+
+	public void OnAttackPerformed()
+	{
+		attackContext.target.AttackReaction();
+		if (attackContext.successful)
+			EffectSpawner.SetEffect("HitEffect", attackContext.target.transform.position + Vector3.up);
+		else
+			EffectSpawner.SetEffect("MISS", attackContext.target.transform.position + Vector3.up);
+	}
+
+	public void DestroySelf()
+	{
+		Destroy(me.gameObject);
+	}
+
+	public void OnDieAnimationEnd()
+	{
+		me.Die();
+	}
+
+	void AttackReaction()
+	{
+		if (me.IsDead())
+		{
+			anim.SetTrigger("Die");
+		}
+		else
+		{
+			anim.SetTrigger("Hit");
+		}
+	}
+}
+
+public struct AttackContext
+{
+	public CharacterAnimation target;
+	public bool successful;
+}
---
+++ Assets/Scripts/Animation/EffectSpawner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class EffectSpawner : MonoBehaviour
+{
+	private static EffectSpawner _instance;
+	private Dictionary<string, List<GameObject>> effects;
+	private Dictionary<string, GameObject> originals;
+
+	static EffectSpawner instance
+	{
+		get
+		{
+			if (_instance) return _instance;
+			var go = new GameObject("Effect Spawner");
+			_instance = go.AddComponent<EffectSpawner>();
+			return _instance;
+		}
+	}
+
+	public static GameObject GetEffect(string effectName)
+	{
+		if (!instance.effects.ContainsKey(effectName))
+			instance.effects.Add(effectName, new List<GameObject>());
+		var idleEffect = instance.effects[effectName].FirstOrDefault(g => !g.activeSelf);
+		if (idleEffect)
+			return idleEffect;
+		var instanciated = InstantiateEffect(effectName);
+		instance.effects[effectName].Add(instanciated);
+		instanciated.transform.SetParent(instance.transform, false);
+		return instanciated;
+	}
+
+	public static void SetEffect(string effectName, Vector3 position)
+	{
+		var effect = GetEffect(effectName);
+		effect.transform.position = position;
+		effect.SetActive(true);
+	}
+
+	static GameObject InstantiateEffect(string effectName)
+	{
+		if (!instance.originals.ContainsKey(effectName))
+		{
+			var loaded = Resources.Load<GameObject>("Effects/" + effectName);
+			instance.originals.Add(effectName, loaded);
+		}
+		return Object.Instantiate<GameObject>(instance.originals[effectName]);
+	}
+
+}
