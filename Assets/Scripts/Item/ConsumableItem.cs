902989a7945552bb4a8f655745632f99535c8d9a
unknown <>
2017-08-13 00:12:02 +0900

    item base code

---
+++ Assets/Scripts/Item/ConsumableItem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+public enum ConsumableItemCode
+{
+    CAKE,
+    JEON,
+    POTION
+}
+
+public class ConsumableItem
+{
+    public virtual void  use()
+    {
+
+    }
+}
+
+public class HealingItem : ConsumableItem
+{
+    public int healAmount;
+    public override void use()
+    {
+        MainLogic.instance.hero.HP += healAmount;
+    }
+}
+
+public class BuffItem : ConsumableItem
+{
+    int turn;
+    public override void use()
+    {
+        MainLogic.instance.hero.buffedTurn += turn;
+    }
+}
---
+++ Assets/Scripts/Item/EquipmentItem.cs
@@ -0,0 +1,34 @@
+﻿public class EquipmentItem
+{
+    public int power;
+}
+
+public class Helmet : EquipmentItem
+{
+
+}
+
+public class Gauntlet : EquipmentItem
+{
+
+}
+
+public class Armor : EquipmentItem
+{
+
+}
+
+public class Shield : EquipmentItem
+{
+
+}
+
+public class Boots : EquipmentItem
+{
+
+}
+
+public class Weapon : EquipmentItem
+{
+
+}
