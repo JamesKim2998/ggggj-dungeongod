e066582c96a47e90e0a987578c23b33c2e5cd751
김대원 <eodnjs2998@gmail.com>
2017-08-13 10:55:38 +0900

    여신상 world position으로 작동하도록 변경.

--- Assets/Scripts/Triggers/GoddessStatue.cs
+++ Assets/Scripts/Triggers/GoddessStatue.cs
@@ -8,19 +8,14 @@ public enum StatueType
 
 public class GoddessStatue : Trigger
 {
-    public Coord coord;
     public StatueType type;
     public int buffDistance; //여신상 버프 범위
     public int healAmount;
 
-    private void Awake()
-    {
-        coord = Coord.Round(transform.localPosition);
-    }
-
     public override void Act()
     {
-        Hero hero = MainLogic.instance.hero;
+        var hero = MainLogic.instance.hero;
+        var coord = Coord.Round(transform.position);
         if (Coord.distance(hero.coord, coord) <= buffDistance)
         {
             Debug.Log("Goddess Statue Activated");
